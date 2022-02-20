923bd4d6e33664aecf7a3dc3853b5b544d8890f2
muthuni-dealwis <kkmdealwis@gmail.com>
2022-02-20 14:19:41 +0530

    Created Gallery and Introduction

---
+++ gallery.php
@@ -0,0 +1,24 @@
+<?php include 'header.php';?>
+<div class="container">
+
+       <h1 class="title"><b>Gallery</b></h1>
+       <div class="row gallery">
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/arunachalam3.jpg" title="Arunachalam Hall" class="gallery-image" data-gallery><img src="images/photos/arunachalam3.jpg" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/nell3.png" title="Akbar Nell Hall" class="gallery-image" data-gallery><img src="images/photos/nell3.png" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/jayathilake3.png" title="Jayathilake Hall" class="gallery-image" data-gallery><img src="images/photos/jayathilake3.png" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/mars3.jpg" title="Marrs Hall" class="gallery-image" data-gallery><img src="images/photos/mars3.jpg" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/marcus3.png" title="Marcus Hall" class="gallery-image" data-gallery><img src="images/photos/marcus3.png" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/newakbar3.png" title="New Akbar Hall" class="gallery-image" data-gallery><img src="images/photos/newakbar3.png" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/jamesperis3.jpg" title="James Peris Hall" class="gallery-image" data-gallery><img src="images/photos/jamesperis3.jpg" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/ivorjenings3.jpg" title="Ivor Jennings Hall" class="gallery-image" data-gallery><img src="images/photos/ivorjenings3.jpg" class="img-responsive"></a></div>             
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/ramanathan3.jpeg" title="Ramanathan Hall" class="gallery-image" data-gallery><img src="images/photos/ramanathan3.jpeg" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/sangamitta3.png" title="Sangamitta Hall" class="gallery-image" data-gallery><img src="images/photos/sangamitta3.png" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/hilda3.jpg" title="Hilda Obeysekara Hall" class="gallery-image" data-gallery><img src="images/photos/hilda3.jpg" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/wijewardana3.png" title="Wijewardana Hall" class="gallery-image" data-gallery><img src="images/photos/wijewardana3.png" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/sarachchandra3.png" title="Ediriweera Sarachchandra Hall and Gunapala Malalasekara Hall" class="gallery-image" data-gallery><img src="images/photos/sarachchandra3.png" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/Sarasaviuyana3.jpg" title="Sarasaviuyana Hall" class="gallery-image" data-gallery><img src="images/photos/Sarasaviuyana3.jpg" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/sarasavimadura3.png" title="Sarasavi Madura Hall" class="gallery-image" data-gallery><img src="images/photos/sarasavimadura3.png" class="img-responsive"></a></div>
+              <!-- <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/senakabibile3.png" title="Senaka Bibile Hall" class="gallery-image" data-gallery><img src="images/photos/senakabibile3.png" class="img-responsive"></a></div> -->
+       </div>
+</div>
+<?php include 'footer.php';?>
---
+++ introduction.php
@@ -0,0 +1,159 @@
+<?php include 'header.php';?>
+<div class="container">
+
+       <h1 class="title"><b>Introduction</b></h1>
+       <div class="row">
+              <div><p>University of Peradeniya is a leading university in Sri Lanka which facilitates about 9605 undergraduates in 9 different faculties.
+	Student Services Branch in the University of Peradeniya provides a comprehensive service to selected number of students from each faculty by administrating and allocating 20 hostels in the university premises. There are 10 male hostels, 8 female hostels and 2 bikku hostels which provide accommodation for around 5500 undergraduates which is nearly 55% of the student population.
+	Wardens and sub wardens from the academic staff are appointed by the university for different halls of residence to maintain the discipline in the hostels. Since there are large number of hostels and students, maintaining these records is a difficult task. 
+       The purpose of this project is to design a database management system for hostels in the university to avoid manual data entry and keep records in a proper manner. This database system maintains information about the hostel, details about the management, staff and students in the hostel so the information can be accessed and manipulated easily.</p></div>
+              <!-- <div class="col-sm-4"><p>Fale acid rest bock, " trappist biere de garde," infusion oxidized, carboy. hard cider, crystal malt hoppy pitch alpha acid dry stout tulip glass fermentation. bitter malt extract, dunkle, squares. units of bitterness trappist wort chiller keg krausen ester. brewpub. cold filter length, alcohol copper degrees plato aau.</p></div>
+              <div class="col-sm-4"><p>Dry stout aau pilsner. bock cold filter lauter tun specific gravity degrees plato ibu cold filter. brew kettle crystal malt, hard cider ipa bright beer caramel malt. pitching aerobic keg finishing hops pub units of bitterness.</p></div>               -->
+       </div>
+
+       <table class="hostels">
+              <thead>
+                     <tr>
+                            <th>No.</th>
+                            <th>Type</th>
+                            <th>Halls of residence</th>
+                            <th>Capacity</th>
+                     </tr>
+              </thead>
+
+              <tbody>
+                     <tr>
+                            <td>1</td>
+                            <td rowspan="10">Male</td>
+                            <td>Arunachalam Hall</td>
+                            <td>189</td>
+                     </tr>
+
+                     <tr>
+                            <td>2</td>
+                            <td>Akbar Nell Hall</td>
+                            <td>598</td>
+                     </tr>
+
+                     <tr>
+                            <td>3</td>
+                            <td>Jayathilake Hall</td>
+                            <td>189</td>
+                     </tr>
+
+                     <tr>
+                            <td>4</td>
+                            <td>Marrs Hall</td>
+                            <td>240</td>
+                     </tr>
+
+                     <tr>
+                            <td>5</td>
+                            <td>Marcus Fernando Hall</td>
+                            <td>246</td>
+                     </tr>
+
+                     <tr>
+                            <td>6</td>
+                            <td>New Akbar Hall</td>
+                            <td>129</td>
+                     </tr>
+
+                     <tr>
+                            <td>7</td>
+                            <td>Hindagala Hall</td>
+                            <td>56</td>
+                     </tr>
+
+                     <tr>
+                            <td>8</td>
+                            <td>James Peris Hall</td>
+                            <td>222</td>
+                     </tr>
+
+                     <tr>
+                            <td>9</td>
+                            <td>Ivor Jennings Hall</td>
+                            <td>508</td>
+                     </tr>
+
+                     <tr>
+                            <td>10</td>
+                            <td>Senaka Bibile Hall</td>
+                            <td>N/A</td>
+                     </tr>
+
+                     <tr>
+                            <td>11</td>
+                            <td rowspan="8">Female</td>
+                            <td>Ramanathan Hall</td>
+                            <td>1022</td>
+                     </tr>
+
+                     <tr>
+                            <td>12</td>
+                            <td>Sangamitta Hall</td>
+                            <td>708</td>
+                     </tr>
+
+                     <tr>
+                            <td>13</td>
+                            <td>Hida Obeysekara Hall</td>
+                            <td>588</td>
+                     </tr>
+
+                     <tr>
+                            <td>14</td>
+                            <td>Wijewardana Hall</td>
+                            <td>732</td>
+                     </tr>
+
+                     <tr>
+                            <td>15</td>
+                            <td>Gunapala Malalasekara</td>
+                            <td>N/A</td>
+                     </tr>
+
+                     <tr>
+                            <td>16</td>
+                            <td>Ediriweera Sarachchandra</td>
+                            <td>N/A</td>
+                     </tr>
+
+                     <tr>
+                            <td>17</td>
+                            <td>Sarasavi Madura Hall</td>
+                            <td>N/A</td>
+                     </tr>
+
+                     <tr>
+                            <td>18</td>
+                            <td>Sarasaviuyana Hall</td>
+                            <td>56</td>
+                     </tr>
+
+                     <tr>
+                            <td>19</td>
+                            <td rowspan="2">Bikku</td>
+                            <td>Wilagedara Bikku Hostel</td>
+                            <td>61</td>
+                     </tr>
+
+                     <tr>
+                            <td>20</td>
+                            <td>Kehelpannala Bikku Hostel</td>
+                            <td>18</td>
+                     </tr>
+
+              </tbody>
+       </table>
+
+       <div class="spacer">
+       <!-- <div class="embed-responsive embed-responsive-16by9"><iframe  class="embed-responsive-item" src="videos/University Of Peradeniya.mp4" width="100%" height="400" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div> -->
+       </div>
+
+
+
+
+</div>
+<?php include 'footer.php';?>
