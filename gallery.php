597b47e2ef6d863a39a92fe3ae808e867c6a97b2
muthuni-dealwis <kkmdealwis@gmail.com>
2022-02-20 18:23:27 +0530

    Added titles in gallery

--- gallery.php
+++ gallery.php
@@ -3,21 +3,66 @@
 
        <h1 class="title"><b>Gallery</b></h1>
        <div class="row gallery">
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/arunachalam3.jpg" title="Arunachalam Hall" class="gallery-image" data-gallery><img src="images/photos/arunachalam3.jpg" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/nell3.png" title="Akbar Nell Hall" class="gallery-image" data-gallery><img src="images/photos/nell3.png" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/jayathilake3.png" title="Jayathilake Hall" class="gallery-image" data-gallery><img src="images/photos/jayathilake3.png" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/mars3.jpg" title="Marrs Hall" class="gallery-image" data-gallery><img src="images/photos/mars3.jpg" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/marcus3.png" title="Marcus Hall" class="gallery-image" data-gallery><img src="images/photos/marcus3.png" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/newakbar3.png" title="New Akbar Hall" class="gallery-image" data-gallery><img src="images/photos/newakbar3.png" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/jamesperis3.jpg" title="James Peris Hall" class="gallery-image" data-gallery><img src="images/photos/jamesperis3.jpg" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/ivorjenings3.jpg" title="Ivor Jennings Hall" class="gallery-image" data-gallery><img src="images/photos/ivorjenings3.jpg" class="img-responsive"></a></div>             
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/ramanathan3.jpeg" title="Ramanathan Hall" class="gallery-image" data-gallery><img src="images/photos/ramanathan3.jpeg" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/sangamitta3.png" title="Sangamitta Hall" class="gallery-image" data-gallery><img src="images/photos/sangamitta3.png" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/hilda3.jpg" title="Hilda Obeysekara Hall" class="gallery-image" data-gallery><img src="images/photos/hilda3.jpg" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/wijewardana3.png" title="Wijewardana Hall" class="gallery-image" data-gallery><img src="images/photos/wijewardana3.png" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/sarachchandra3.png" title="Ediriweera Sarachchandra Hall and Gunapala Malalasekara Hall" class="gallery-image" data-gallery><img src="images/photos/sarachchandra3.png" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/Sarasaviuyana3.jpg" title="Sarasaviuyana Hall" class="gallery-image" data-gallery><img src="images/photos/Sarasaviuyana3.jpg" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/sarasavimadura3.png" title="Sarasavi Madura Hall" class="gallery-image" data-gallery><img src="images/photos/sarasavimadura3.png" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/arunachalam3.jpg" title="Arunachalam Hall" class="gallery-image" data-gallery>
+                     <img src="images/photos/arunachalam3.jpg" class="img-responsive">
+                     <figcaption>Arunachalam Hall </figcaption></a></div>
+                     
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/nell3.png" title="Akbar Nell Hall" class="gallery-image" data-gallery>
+                     <img src="images/photos/nell3.png" class="img-responsive">
+                     <figcaption > Akbar Nell Hall </figcaption></a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/jayathilake3.png" title="Jayathilake Hall" class="gallery-image" data-gallery>
+                     <img src="images/photos/jayathilake3.png" class="img-responsive">
+                     <figcaption> Jayathilake Hall </figcaption></a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/mars3.jpg" title="Marrs Hall" class="gallery-image" data-gallery>
+                     <img src="images/photos/mars3.jpg" class="img-responsive">
+                     <figcaption> Marrs Hall </figcaption></a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/marcus3.png" title="Marcus Hall" class="gallery-image" data-gallery>
+                     <img src="images/photos/marcus3.png" class="img-responsive">
+                     <figcaption> Marcus Hall </figcaption></a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/newakbar3.png" title="New Akbar Hall" class="gallery-image" data-gallery>
+                     <img src="images/photos/newakbar3.png" class="img-responsive">
+                     <figcaption> New Akbar Hall </figcaption></a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/jamesperis3.jpg" title="James Peris Hall" class="gallery-image" data-gallery>
+                     <img src="images/photos/jamesperis3.jpg" class="img-responsive">
+                     <figcaption> James Peris Hall </figcaption></a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/ivorjenings3.jpg" title="Ivor Jennings Hall" class="gallery-image" data-gallery>
+                     <img src="images/photos/ivorjenings3.jpg" class="img-responsive">
+                     <figcaption> Ivor Jennings Hall </figcaption></a></div> 
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/ramanathan3.jpeg" title="Ramanathan Hall" class="gallery-image" data-gallery>
+                     <img src="images/photos/ramanathan3.jpeg" class="img-responsive">
+                     <figcaption> Ramanathan Hall </figcaption></a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/sangamitta3.png" title="Sangamitta Hall" class="gallery-image" data-gallery>
+                     <img src="images/photos/sangamitta3.png" class="img-responsive">
+                     <figcaption> Sangamitta Hall </figcaption></a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/hilda3.jpg" title="Hilda Obeysekara Hall" class="gallery-image" data-gallery>
+                     <img src="images/photos/hilda3.jpg" class="img-responsive">
+                     <figcaption> Hilda Obeysekara Hall </figcaption></a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/wijewardana3.png" title="Wijewardana Hall" class="gallery-image" data-gallery>
+                     <img src="images/photos/wijewardana3.png" class="img-responsive">
+                     <figcaption> Wijewardana Hall </figcaption></a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/sarachchandra3.png" title="Ediriweera Sarachchandra Hall and Gunapala Malalasekara Hall" class="gallery-image" data-gallery>
+                     <img src="images/photos/sarachchandra3.png" class="img-responsive">
+                     <figcaption> Ediriweera Sarachchandra Hall and Gunapala Malalasekara Hall </figcaption></a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/Sarasaviuyana3.jpg" title="Sarasaviuyana Hall" class="gallery-image" data-gallery>
+                     <img src="images/photos/Sarasaviuyana3.jpg" class="img-responsive">
+                     <figcaption> Sarasaviuyana Hall </figcaption></a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/sarasavimadura3.png" title="Sarasavi Madura Hall" class="gallery-image" data-gallery>
+                     <img src="images/photos/sarasavimadura3.png" class="img-responsive">
+                     <figcaption> Sarasavi Madura Hall </figcaption></a></div>
+
               <!-- <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/senakabibile3.png" title="Senaka Bibile Hall" class="gallery-image" data-gallery><img src="images/photos/senakabibile3.png" class="img-responsive"></a></div> -->
        </div>
 </div>
