33f0a91f099549f2202861fcbf824441271a74da
sheizz <e18203@eng.pdn.ac.lk>
2022-05-25 00:02:13 +0530

    ...

---
+++ Admin_Warden_Subwarden/studentDetails.php
@@ -0,0 +1,275 @@
+<!doctype html>
+<html lang="en" class="no-js">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Student Details</title>
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/dataTables.bootstrap.min.css">>
+    <link rel="stylesheet" href="css/bootstrap-social.css">
+    <link rel="stylesheet" href="css/bootstrap-select.css">
+    <link rel="stylesheet" href="css/fileinput.min.css">
+    <link rel="stylesheet" href="css/awesome-bootstrap-checkbox.css">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+    <?php include('includes/header.php'); ?>
+    <div class="ts-main-content">
+        <?php include('includes/sidebar.php'); ?>
+        <div class="content-wrapper">
+            <div class="container-fluid">
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2 class="page-title">Student Details </h2>
+                        <form method="post" action="" name="registration" class="form-horizontal">
+
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="panel panel-primary">
+
+                                        <div class="panel-heading">Student Details</div>
+                                        <div class="panel-body">
+
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label"> NIC : </label>
+                                                <div class="col-sm-8">
+                                                    <input type="search" name="stu_nic" id="stu_nic" class="form-control" required="required">
+                                                </div>
+                                                <button type="button" name="stu_search" id="stu_search" class="btn btn-primary"> Search </button>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label"> Student ID : </label>
+                                                <div class="col-sm-8">
+                                                    <input type="text" name="stu_id" id="stu_id" class="form-control" required="required">
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label"> First Name : </label>
+                                                <div class="col-sm-8">
+                                                    <input type="text" name="stu_fname" id="stu_fname" class="form-control" required="required">
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label"> Last Name : </label>
+                                                <div class="col-sm-8">
+                                                    <input type="text" name="stu_lname" id="stu_lname" class="form-control" required="required">
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label"> Gender : </label>
+                                                <div class="col-sm-8">
+                                                    <select name="stu_gender" id="stu_gender" class="form-control" required="required">
+                                                        <option value="male">Male</option>
+                                                        <option value="female">Female</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label"> Birth Date : </label>
+                                                <div class="col-sm-8">
+                                                    <input type="date" name="stu_bdate" id="stu_bdate" class="form-control" required="required">
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label"> Contact No : </label>
+                                                <div class="col-sm-8">
+                                                    <input type="tel" name="stu_contact" id="stu_contact" class="form-control" pattern="[0-9]{3}-[0-9]{7}" required="required">
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label"> Address : </label>
+                                                <div class="col-sm-8">
+                                                    <input type="text" name="stu_address" id="stu_address" class="form-control" required="required">
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label"> Faculty : </label>
+                                                <div class="col-sm-8">
+                                                    <select name="stu_faculty" id="stu_faculty" class="form-control" required="required">
+                                                        <option value="agriculture">Faculty of Agriculture</option>
+                                                        <option value="ahs">Faculty of Allied Health Sciences</option>
+                                                        <option value="arts">Faculty of Arts</option>
+                                                        <option value="dental">Faculty of Dental Sciences</option>
+                                                        <option value="engineering">Faculty of Engineering</option>
+                                                        <option value="management">Faculty of Management</option>
+                                                        <option value="medicine">Faculty of Medicine</option>
+                                                        <option value="science">Faculty of Science</option>
+                                                        <option value="vet">Faculty of Veterinary Medicine and Animal Science</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label"> Batch : </label>
+                                                <div class="col-sm-8">
+                                                    <select name="stu_batch" id="stu_batch" class="form-control" required="required">
+                                                        <option value="b20">20 Btach</option>
+                                                        <option value="b19">19 Btach</option>
+                                                        <option value="b18">18 Btach</option>
+                                                        <option value="b17">17 Btach</option>
+                                                        <option value="b16">16 Btach</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="panel panel-primary">
+
+                                        <div class="panel-heading">Hostel Details</div>
+                                        <div class="panel-body">
+
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label"> Hostel : </label>
+                                                <div class="col-sm-8">
+                                                    <select name="stu_hostel" id="stu_hostel" class="form-control" required="required">
+                                                        <option value="arunachalam_hall">Arunachalam Hall</option>
+                                                        <option value="akbarnell_hall">Akbar Nell Hall</option>
+                                                        <option value="newakbar_hall">New Akbar Hall</option>
+                                                        <option value="marrs_hall">Marrs Hall</option>
+                                                        <option value="marcus_hall">Marcus Fernando Hall</option>
+                                                        <option value="jayathilake_hall">Jayathilake Hall</option>
+                                                        <option value="hindagala_hall">Hindagala Hall</option>
+                                                        <option value="jamesperis_hall">James Peris Hall</option>
+                                                        <option value="ivorjennings_hall">Ivor Jennings Hall</option>
+                                                        <option value="senakabibile_hall">Senaka Bibile Hall</option>
+                                                        <option value="ramanathan_hall">Ramanathan Hall</option>
+                                                        <option value="sangamitta_hall">Sangamitta Hall</option>
+                                                        <option value="hildaobeysekara_hall">Hilda Obeysekara Hall</option>
+                                                        <option value="wijewardana_hall">Wijewardana Hall</option>
+                                                        <option value="gunapalamalalasekara_hall">Gunapala Malalasekara</option>
+                                                        <option value="sarachchandra_hall">Ediriweera Sarachchandra</option>
+                                                        <option value="sarasavimadura_hall">Sarasavi Madura Hall</option>
+                                                        <option value="sarasaviuyana_hall">Sarasaviuyana Hall</option>
+                                                        <option value="wilegadara_hall">Wilagedara Bikku Hostel</option>
+                                                        <option value="kehelpannala_hall">Kehelpannala Bikku Hostel</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label"> Room No : </label>
+                                                <div class="col-sm-8">
+                                                    <input type="text" name="stu_room_no" id="stu_room_no" class="form-control" required="required">
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label"> Check-In Date : </label>
+                                                <div class="col-sm-8">
+                                                    <input type="date" name="stu_checkin" id="stu_checkin" class="form-control" required="required">
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label"> Check-Out Date : </label>
+                                                <div class="col-sm-8">
+                                                    <input type="date" name="stu_checkout" id="stu_checkout" class="form-control" required="required">
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label"> Amount Paid : </label>
+                                                <div class="col-sm-8">
+                                                    <input type="text" name="stu_amount" id="stu_amount" class="form-control" required="required">
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="panel panel-primary">
+
+                                        <div class="panel-heading">Guardian Details</div>
+                                        <div class="panel-body">
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label"> First Name : </label>
+                                                <div class="col-sm-8">
+                                                    <input type="text" name="gur_fname" id="gur_fname" class="form-control" required="required">
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label"> Last Name : </label>
+                                                <div class="col-sm-8">
+                                                    <input type="text" name="gur_lname" id="gur_lname" class="form-control" required="required">
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label"> NIC : </label>
+                                                <div class="col-sm-8">
+                                                    <input type="text" name="gur_nic" id="gur_nic" class="form-control" required="required">
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label"> Contact No : </label>
+                                                <div class="col-sm-8">
+                                                    <input type="tel" name="gur_contact" id="gur_contact" class="form-control" pattern="[0-9]{3}-[0-9]{7}" required="required">
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label"> Email : </label>
+                                                <div class="col-sm-8">
+                                                    <input type="email" name="gur_email" id="gur_email" class="form-control" required="required">
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label"> Ocuupation : </label>
+                                                <div class="col-sm-8">
+                                                    <input type="text" name="gur_ocuupation" id="gur_ocuupation" class="form-control" required="required">
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label"> Relationship : </label>
+                                                <div class="col-sm-8">
+                                                    <input type="text" name="gur_relationship" id="gur_relationship" class="form-control" required="required">
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-8 col-sm-offset-4">
+                                <input type="submit" id="stu_update" name="stu_update" Value="Update" class="btn btn-primary">
+                                <button type="button" name="stu_delete" id="stu_delete" class="btn btn-primary"> Delete </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>
+</body>
+
+
+</html>
--- gallery.php
+++ gallery.php
@@ -1,20 +1,85 @@
-<?php include 'header.php';?>
+<?php include 'header.php'; ?>
 <div class="container">
 
-       <h1 class="title"><b>Gallery</b></h1>
+       <h1 class="title"><b>Photo Gallery</b></h1>
        <div class="row gallery">
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/1.jpg" title="Foods" class="gallery-image" data-gallery><img src="images/photos/1.jpg" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/2.jpg" title="Coffee" class="gallery-image" data-gallery><img src="images/photos/2.jpg" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/3.jpg" title="Travel" class="gallery-image" data-gallery><img src="images/photos/3.jpg" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/4.jpg" title="Adventure" class="gallery-image" data-gallery><img src="images/photos/4.jpg" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/5.jpg" title="Fruits" class="gallery-image" data-gallery><img src="images/photos/5.jpg" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/6.jpg" title="Summer" class="gallery-image" data-gallery><img src="images/photos/6.jpg" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/7.jpg" title="Bathroom" class="gallery-image" data-gallery><img src="images/photos/7.jpg" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/8.jpg" title="Rooms" class="gallery-image" data-gallery><img src="images/photos/8.jpg" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/9.jpg" title="Big Room" class="gallery-image" data-gallery><img src="images/photos/9.jpg" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/11.jpg" title="Living Room" class="gallery-image" data-gallery><img src="images/photos/11.jpg" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/1.jpg" title="Fruits" class="gallery-image" data-gallery><img src="images/photos/1.jpg" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/3.jpg" title="Travel" class="gallery-image" data-gallery><img src="images/photos/3.jpg" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/arunachalam3.jpg" title="Arunachalam Hall" class="gallery-image" data-gallery>
+                            <img src="images/photos/arunachalam3.jpg" class="img-responsive">
+                            <figcaption class="hostelName">Arunachalam Hall</figcaption>
+                     </a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/nell3.png" title="Akbar Nell Hall" class="gallery-image" data-gallery>
+                            <img src="images/photos/nell3.png" class="img-responsive">
+                            <figcaption class="hostelName"> Akbar Nell Hall </figcaption>
+                     </a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/jayathilake3.png" title="Jayathilake Hall" class="gallery-image" data-gallery>
+                            <img src="images/photos/jayathilake3.png" class="img-responsive">
+                            <figcaption class="hostelName"> Jayathilake Hall </figcaption>
+                     </a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/mars3.jpg" title="Marrs Hall" class="gallery-image" data-gallery>
+                            <img src="images/photos/mars3.jpg" class="img-responsive">
+                            <figcaption class="hostelName"> Marrs Hall </figcaption>
+                     </a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/marcus3.png" title="Marcus Hall" class="gallery-image" data-gallery>
+                            <img src="images/photos/marcus3.png" class="img-responsive">
+                            <figcaption class="hostelName"> Marcus Hall </figcaption>
+                     </a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/newakbar3.png" title="New Akbar Hall" class="gallery-image" data-gallery>
+                            <img src="images/photos/newakbar3.png" class="img-responsive">
+                            <figcaption class="hostelName"> New Akbar Hall </figcaption>
+                     </a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/jamesperis3.jpg" title="James Peris Hall" class="gallery-image" data-gallery>
+                            <img src="images/photos/jamesperis3.jpg" class="img-responsive">
+                            <figcaption class="hostelName"> James Peris Hall </figcaption>
+                     </a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/ivorjenings3.jpg" title="Ivor Jennings Hall" class="gallery-image" data-gallery>
+                            <img src="images/photos/ivorjenings3.jpg" class="img-responsive">
+                            <figcaption class="hostelName"> Ivor Jennings Hall </figcaption>
+                     </a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/ramanathan3.jpeg" title="Ramanathan Hall" class="gallery-image" data-gallery>
+                            <img src="images/photos/ramanathan3.jpeg" class="img-responsive">
+                            <figcaption class="hostelName"> Ramanathan Hall </figcaption>
+                     </a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/sangamitta3.png" title="Sangamitta Hall" class="gallery-image" data-gallery>
+                            <img src="images/photos/sangamitta3.png" class="img-responsive">
+                            <figcaption class="hostelName"> Sangamitta Hall </figcaption>
+                     </a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/hilda3.jpg" title="Hilda Obeysekara Hall" class="gallery-image" data-gallery>
+                            <img src="images/photos/hilda3.jpg" class="img-responsive">
+                            <figcaption class="hostelName"> Hilda Obeysekara Hall </figcaption>
+                     </a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/wijewardana3.png" title="Wijewardana Hall" class="gallery-image" data-gallery>
+                            <img src="images/photos/wijewardana3.png" class="img-responsive">
+                            <figcaption class="hostelName"> Wijewardana Hall </figcaption>
+                     </a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/sarachchandra3.png" title="Ediriweera Sarachchandra Hall and Gunapala Malalasekara Hall" class="gallery-image" data-gallery>
+                            <img src="images/photos/sarachchandra3.png" class="img-responsive">
+                            <figcaption class="hostelName"> Ediriweera Sarachchandra Hall and Gunapala Malalasekara Hall </figcaption>
+                     </a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/Sarasaviuyana3.jpg" title="Sarasaviuyana Hall" class="gallery-image" data-gallery>
+                            <img src="images/photos/Sarasaviuyana3.jpg" class="img-responsive">
+                            <figcaption class="hostelName"> Sarasaviuyana Hall </figcaption>
+                     </a></div>
+
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/sarasavimadura3.png" title="Sarasavi Madura Hall" class="gallery-image" data-gallery>
+                            <img src="images/photos/sarasavimadura3.png" class="img-responsive">
+                            <figcaption class="hostelName"> Sarasavi Madura Hall </figcaption>
+                     </a></div>
+
+              <!-- <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/senakabibile3.png" title="Senaka Bibile Hall" class="gallery-image" data-gallery><img src="images/photos/senakabibile3.png" class="img-responsive"></a></div> -->
        </div>
+
 </div>
-<?php include 'footer.php';?>
+<?php include 'footer.php'; ?>
